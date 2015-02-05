3bc3908799cd1dbe423092e72c3cdccfd90ba970
Connor <cbb692@yahoo.com>
2015-02-05 14:39:02 -0500

    Added the 2D version of the project -CB

---
+++ wizards2D/Assets/Scripts/Element.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Element : MonoBehaviour {
+	int dmg; //damage mod for element
+	int speed; //projectile mod for element
+	int cast; //cast point mod for element
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
