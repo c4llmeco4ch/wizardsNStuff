763ebeb31dc617fb1eaa6c0a1fdf5a8e5e12d4f7
Connor <cbb692@yahoo.com>
2015-02-10 15:54:43 -0500

    FixedUpdate() for slash written -CB

--- wizards2D/Assets/Scripts/Slash.cs
+++ wizards2D/Assets/Scripts/Slash.cs
@@ -5,14 +5,23 @@ using System.Collections;
 public class Slash : Spell {
 	PlayerChar p; //the player who is casting this slash
 	bool casting; //whether the slash is currently in effect
+	int framesLeft;
+	BoxCollider sword;
 	public void FixedUpdate(){
-		
+		if(Input.GetButtonDown("Player"+p.playerNum+"_Spell_Slash_Mac") && !casting)
+			Start ();
+		else if(framesLeft==0){
+			casting=false;
+			Destroy(sword);
+		}
+		else if(framesLeft>0)
+			framesLeft--;	
 	}
 	
 	public void Start(){//call this method when you wish to cast the slash
 		cast(p);
 		casting=true;
-		
+		framesLeft=64;
 	}
 	
 	public void prepSlash(PlayerChar p){this.p=p;}//call this immediately after creating this object
@@ -29,7 +38,7 @@ public class Slash : Spell {
 		float y=p.transform.position.y;
 		float x=p.transform.position.x;
 		float z=p.transform.position.z;
-		BoxCollider sword = new BoxCollider();
+		sword = new BoxCollider();
 		sword.size=new Vector3((float)this.getRange(),(float)this.getRange(),(float)this.getRange());
 		if(p.facingRight){
 			x=p.collider.bounds.size.x/2+x;
@@ -44,13 +53,13 @@ public class Slash : Spell {
 	
 	//defines what happens when an object collides with a given spell
 	override public void onCollisionEnter(Collision c){
-		if(c.gameObject.name.Contains("Player")){
+		/*if(c.gameObject.name.Contains("Player")){
 			PlayerChar p=c.gameObject.GetComponent(PlayerChar);//issues grabbing the playerchar from the gameobject
 			p.takeDamage(this.getDmg(),this);
 		}
 		else if(c.gameObject.GetType()==Spell){ //same as above
 			Spell s=c.gameObject;
-		}
+		}*/
 			
 	}
 	
