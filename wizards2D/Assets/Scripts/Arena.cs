ce31eef5333051eee3e5a373d0d162a8c6256fa6
Connor <cbb692@yahoo.com>
2015-02-20 15:32:09 -0500

    Updated Arena to hold if all players are dead -CB

--- wizards2D/Assets/Scripts/Arena.cs
+++ wizards2D/Assets/Scripts/Arena.cs
@@ -2,17 +2,31 @@
 using System.Collections;
 
 public class Arena : MonoBehaviour {
-	int height; int width; //define size of board
-	int pNum; //number of players in game
-	bool[] alive; //for each player in game, true if alive, else false
-	int[][]pCoor; //for each player in game, x,y coordinates for each player (-10,-10 if dead)
-	// Use this for initialization
-	void Start () {
+	private static Arena _instance;
+	public static Arena instance {get {return _instance;}}	
 	
+	int pNum; //number of players in game
+	PlayerChar[] pc; //States of each player
+	
+	//Called upon instantiation
+	public void Awake(){
+		if(_instance!=null) Destroy(_instance.gameObject);
+		_instance=this;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public void Update () {
+		int allDead=0;
+		foreach(PlayerChar p in pc){
+			if(p.isDead) 
+				allDead++;
+		}
+		if(allDead>1)
+			gameOver();
+	}
 	
+	//Called when all but one player are dead
+	public void gameOver(){
+		//do stuff
 	}
 }
