f0d514976c0c1d2ab819709fc28872538248cd63
unixninja92 <charles@unixninja92.com>
2015-02-21 02:17:53 -0500

    Started to add scpriting for Element Selection. Also added audio files for slash.

---
+++ wizards2D/Assets/Scripts/ElementSelector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ElementSelector {//: MonoBehaviour {
+	public Button left;
+	public Button right;
+	public Image selectedElementText;
+	public int playerNum;
+	public int elementNum;
+
+	public GameInit struc;
+	private Element selectedElement;
+
+
+	// Use this for initialization
+	void Start () {
+		if(elementNum == 0)
+			selectedElement = new Earth();
+		else
+			selectedElement = new Air();
+		struc = new GameInit ();
+//		struc.;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void Left() {
+
+	}
+
+	public void Right() {
+
+	}
+}
---
+++ wizards2D/Assets/Scripts/GameInit.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInit {
+//	public static Element[][] elementChoices = new Element[4][2];
+//
+//	public static 
+}
