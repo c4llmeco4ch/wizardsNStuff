fdd6b43d196ade2b4ce4077952dede5d74df7508
unixninja92 <charles@unixninja92.com>
2015-03-06 05:17:43 -0500

    Added MainMenu

---
+++ wizards2D/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public void play(bool tutorial) {
+        GameInit.tutorial = tutorial;
+        Application.LoadLevel("ChooseElementScene");
+    }
+}
